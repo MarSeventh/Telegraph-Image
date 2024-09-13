1a6218d48a2342867768d04454478c94aa6baf29
MarSeventh <1193267292@qq.com>
2024-09-13 14:40:58 +0800

    增加admin重定向

--- functions/admin.js
+++ functions/admin.js
@@ -1,4 +1,15 @@
 export async function onRequest(context) {
     // 重定向到/dashboard
-    return Response.redirect("/dashboard", 302)
+    const {
+        request, // same as existing Worker API
+        env, // same as existing Worker API
+        params, // if filename includes [id] or [[path]]
+        waitUntil, // same as ctx.waitUntil in existing Worker API
+        next, // used for middleware or to fetch assets
+        data, // arbitrary space for passing data between middlewares
+      } = context;
+      //get the request url
+      const url = new URL(request.url);
+      //redirect to dashboard page
+      return Response.redirect(url.origin+"/dashboard", 302)
 }
