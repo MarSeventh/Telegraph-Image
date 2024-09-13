54dcf39bfd89f3bdfd14a14caa36bfa099fec40c
MarSeventh <1193267292@qq.com>
2024-09-13 13:13:15 +0800

    优化文件url显示

--- functions/upload.js
+++ functions/upload.js
@@ -149,6 +149,7 @@ export async function onRequestPost(context) {  // Contents of context object
         // 构建独一无二的 ID
         const unique_index = time + Math.floor(Math.random() * 10000);
         const fullId = fileName? unique_index + '_' + fileName : unique_index + '.' + fileExt;
+        const encodedFullId = encodeURIComponent(fullId);
         // 若上传成功，将响应返回给客户端
         if (response.ok) {
             res = new Response(
@@ -162,7 +163,7 @@ export async function onRequestPost(context) {  // Contents of context object
         const apikey = env.ModerateContentApiKey;
     
         if (apikey == undefined || apikey == null || apikey == "") {
-            await env.img_url.put(fullId, "", {
+            await env.img_url.put(encodedFullId, "", {
                 metadata: { FileName: fileName, FileType: fileType, ListType: "None", Label: "None", TimeStamp: time, Channel: "TelegramNew", TgFilePath: filePath, TgFileId: id },
             });
         } else {
@@ -172,7 +173,7 @@ export async function onRequestPost(context) {  // Contents of context object
                     throw new Error(`HTTP error! status: ${fetchResponse.status}`);
                 }
                 const moderate_data = await fetchResponse.json();
-                await env.img_url.put(fullId, "", {
+                await env.img_url.put(encodedFullId, "", {
                     metadata: { FileName: fileName, FileType: fileType, ListType: "None", Label: moderate_data.rating_label, TimeStamp: time, Channel: "TelegramNew", TgFilePath: filePath, TgFileId: id  },
                 });
             } catch (error) {
