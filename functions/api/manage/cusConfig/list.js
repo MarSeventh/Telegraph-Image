ce3c97a2a07d2ff41b1b3df1312bb272c80ef8ef
MarSeventh <1193267292@qq.com>
2025-03-14 16:05:20 +0800

    Feat:用户上传管理支持显示IP具体位置

--- functions/api/manage/cusConfig/list.js
+++ functions/api/manage/cusConfig/list.js
@@ -87,21 +87,30 @@ async function dealByIP(data) {
 // 获取IP地址
 async function getIPAddress(ip) {
     let address = '未知';
-    const ipInfo = await fetch(`https://apimobile.meituan.com/locate/v2/ip/loc?rgeo=true&ip=${ip}`);
-    const ipData = await ipInfo.json();
-    
-    if (ipInfo.ok && ipData.data) {
-        const lng = ipData.data?.lng || 0;
-        const lat = ipData.data?.lat || 0;
+    try {
+        const ipInfo = await fetch(`https://apimobile.meituan.com/locate/v2/ip/loc?rgeo=true&ip=${ip}`);
+        const ipData = await ipInfo.json();
         
-        // 读取具体地址
-        const addressInfo = await fetch(`https://apimobile.meituan.com/group/v1/city/latlng/${lat},${lng}?tag=0`);
-        const addressData = await addressInfo.json();
+        if (ipInfo.ok && ipData.data) {
+            const lng = ipData.data?.lng || 0;
+            const lat = ipData.data?.lat || 0;
+            
+            // 读取具体地址
+            const addressInfo = await fetch(`https://apimobile.meituan.com/group/v1/city/latlng/${lat},${lng}?tag=0`);
+            const addressData = await addressInfo.json();
 
-        if (addressInfo.ok && addressData.data) {
-            address = addressData.data.detail + ', ' + addressData.data.city + ', ' + addressData.data.province + ', ' + addressData.data.country;
+            if (addressInfo.ok && addressData.data) {
+                // 根据各字段是否存在，拼接地址
+                address = [
+                    addressData.data.detail,
+                    addressData.data.city,
+                    addressData.data.province,
+                    addressData.data.country
+                ].filter(Boolean).join(', ');
+            }
         }
+    } catch (error) {
+        console.error('Error fetching IP address:', error);
     }
-
     return address;
 }
